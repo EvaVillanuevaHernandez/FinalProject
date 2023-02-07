7c1a15dc6dc7dd7b47ae39a852db6502a32fc96a
Albebot <albertotrujillocarballo@gmail.com>
2023-02-07 02:26:34 +0000

    commit doctor model interface

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './views/login-page/login-page.component';
 import { AddPatientComponent } from './views/patients/add/add.patient/add.patient.component';
 import { PatientsComponent } from './views/patients/list/patients.component';
+//import { DoctorsComponent } from './views/patients/list/patients.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'listpatients', component: PatientsComponent },
+  //{ path: 'listdoctors', component: DoctorsComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'addpatient', component: AddPatientComponent },
 ];
