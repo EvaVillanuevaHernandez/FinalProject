2e14313af6500a4b77853987c9f72d2a378cc31f
EvaVillanuevaHernandez <evaluna103@gmail.com>
2023-02-16 20:22:31 +0000

    commit some test

--- backend/src/test/java/com/eva/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/eva/backend/BackendApplicationTests.java
@@ -1,60 +1,151 @@
 package com.eva.backend;
 
-import com.eva.backend.entity.models.Doctor;
-import com.eva.backend.entity.services.DoctorServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import com.eva.backend.entity.dao.IDoctorDao;
+import com.eva.backend.entity.dao.IPatientDao;
+import com.eva.backend.entity.dao.IPrescriptionDao;
+import com.eva.backend.entity.models.*;
+import com.eva.backend.entity.services.DoctorServiceImpl;
+import com.eva.backend.entity.services.PatientServiceImpl;
+import com.eva.backend.entity.services.PrescriptionServiceImpl;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.doNothing;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Assertions;
 
-@RunWith(SpringRunner.class)
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
-@AutoConfigureMockMvc
 class BackendApplicationTests {
 
-	@Autowired
-	private MockMvc mvc;
+	@Mock
+	private IPrescriptionDao prescriptionDao;
+	@Mock
+	private IPatientDao patientDao;
+	@Mock
+	private IDoctorDao doctorDao;
+	@InjectMocks
+	private PrescriptionServiceImpl prescriptionService;
+	@InjectMocks
 
-	@MockBean
+	private PatientServiceImpl patientService;
+	@InjectMocks
 	private DoctorServiceImpl doctorService;
-
 	@Test
-	void testGetData() throws Exception {
-		this.mvc.perform(get("/doctors"))
-				.andExpect(status().isOk());
+	public void testGetPrescription() {
+		Prescription prescription = new Prescription();
+		prescription.setId(1);
+		prescription.setPosology("Test Prescription");
+		when(prescriptionDao.findById(1)).thenReturn(Optional.of(prescription));
+		Prescription result = prescriptionService.get(1);
+		assertEquals(1, result.getId());
+		assertEquals("Test Prescription", result.getPosology());
+	}
+	@Test
+	public void testGetPatient() {
+		Patient patient = new Patient();
+		patient.setId(1);
+		patient.setDni("Test Patient");
+		patient.setName("Test Patient");
+		patient.setHistory("Test Patient");
+		when(patientDao.findById(1)).thenReturn(Optional.of(patient));
+		Patient result = patientService.get(1);
+		assertEquals(1, result.getId());
+		assertEquals("Test Patient", result.getDni());
+		assertEquals("Test Patient", result.getName());
+		assertEquals("Test Patient", result.getHistory());
 	}
 
-	@Test
-	void testDeleteSpeaker() throws Exception {
-		int id = 1;
-		doNothing().when(doctorService).delete(id);
+	@DataJpaTest
+	public class DoctorTest {
 
-		this.mvc.perform(delete("/doctors/{id}", id))
-				.andExpect(status().isOk());
+		@Test
+		public void testGettersAndSetters() {
+			Doctor doctor = new Doctor();
+			doctor.setId(1);
+			doctor.setCollegiateNum("1234");
+			doctor.setName("John");
+			doctor.setSurname("Doe");
+			doctor.setSecondSurname("Smith");
+			doctor.setDni("123456789A");
+
+			List<Prescription> prescriptions = new ArrayList<>();
+			prescriptions.add(new Prescription());
+			doctor.setPrescriptions(prescriptions);
+
+			List<Prescription> patients = new ArrayList<>();
+			patients.add(new Prescription());
+			doctor.setPatients(patients);
+
+			User user = new User();
+			doctor.setUser(user);
+
+			Assertions.assertEquals(1, doctor.getId());
+			Assertions.assertEquals("1234", doctor.getCollegiateNum());
+			Assertions.assertEquals("John", doctor.getName());
+			Assertions.assertEquals("Doe", doctor.getSurname());
+			Assertions.assertEquals("Smith", doctor.getSecondSurname());
+			Assertions.assertEquals("123456789A", doctor.getDni());
+			Assertions.assertEquals(prescriptions, doctor.getPrescriptions());
+			Assertions.assertEquals(patients, doctor.getPatients());
+			Assertions.assertEquals(user, doctor.getUser());
+		}
+
+		@Test
+		@Sql({"/sql/create-doctor.sql"})
+		public void testDoctorEntityMapping() {
+			Doctor doctor = new Doctor();
+			doctor.setCollegiateNum("12345");
+			doctor.setName("John");
+			doctor.setSurname("Doe");
+			doctor.setSecondSurname("Smith");
+			doctor.setDni("12345678A");
+
+			assertEquals("12345", doctor.getCollegiateNum());
+			assertEquals("John", doctor.getName());
+			assertEquals("Doe", doctor.getSurname());
+			assertEquals("Smith", doctor.getSecondSurname());
+			assertEquals("12345678A", doctor.getDni());
+		}
 	}
 
-	@Test
-	void testPostSpeaker() throws Exception {
-		Doctor doctor = new Doctor();
-		doctor.setId(1);
-		doctor.setName("Doctor");
-		doctor.setSurname("Ejemplo");
-		doctor.setSecondSurname("Uno");
-		doctor.setDni("453392J");
-		doctor.setCollegiateNum("1-2-2");
+	class UserTest {
 
-		doNothing().when(doctorService).post(doctor);
+		@Test
+		void test() {
+			User user = new User("testUser", "test@example.com", "testPassword");
+			assertEquals("testUser", user.getUsername());
+			assertEquals("test@example.com", user.getEmail());
+			assertEquals("testPassword", user.getPassword());
 
-		this.mvc.perform(post("/doctors"))
-				.andExpect(status().isOk());
+			Set<Role> roles = new HashSet<>();
+			roles.add(new Role());
+			user.setRoles(roles);
+			assertEquals(1, user.getRoles().size());
+		}
+
 	}
 
+
+
+
+
+	@Test
+	public void whenValidId_thenDoctorShouldBeFound() {
+		Doctor alex = new Doctor(1, "collegiateNum", "Alex", "Smith", "Doe", "123456789");
+		when(doctorDao.findById(1)).thenReturn(java.util.Optional.of(alex));
+		Doctor found = doctorService.get(1);
+
+		assertThat(found.getName())
+				.isEqualTo(alex.getName());
+	}
 }
